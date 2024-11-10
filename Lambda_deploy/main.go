77d1a832fb58e4989980b15adc2a4727e0f35f12
yuttto0024 <yuto0024@outlook.com>
2024-11-10 18:19:37 +0900

    Firestoreからuser_profilesの全データを取得する機能の実装

--- Lambda_deploy/main.go
+++ Lambda_deploy/main.go
@@ -7,6 +7,7 @@ import (
 
 	"cloud.google.com/go/firestore"
 	"github.com/joho/godotenv"
+	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 )
 
@@ -22,12 +23,33 @@ func main() {
 	// Firestoreクライアントの設定、初期化
 	ctx := context.Background()
 	client, err = firestore.NewClient(ctx, "peachtech-mokumoku", option.WithCredentialsFile("./peachtech-mokumoku-91af9d3931c9.json"))
-	if err != nil {
-		log.Fatalf("Firestoreクライアントの作成に失敗しました: %v", err)
-	}
-	defer client.Close()
 
-	// クライアント作成成功時にメッセージ
-	fmt.Println("Firestoreクライアントの作成に成功しました")
+	// Firestoreから全データを読み込む
+    ReadAllFromFirestore(ctx)
+}
 
+// Firestoreから全データを読み込む関数
+func ReadAllFromFirestore(ctx context.Context) {
+    // 読み込むコレクションを指定
+    docRefs := client.Collection("user_profiles").Documents(ctx)
+
+    // ドキュメントを反復処理して取得
+    for {
+        docSnap, err := docRefs.Next()
+        if err != nil {
+            // エラーが発生した場合は終了
+            if err == iterator.Done {
+                break
+            }
+            log.Printf("Firestoreからのデータ取得エラー: %v", err)
+            return
+        }
+
+  // ドキュメントのデータを取得し、コンソールに表示
+        data := docSnap.Data() // Data() でマップとして取得
+        if len(data) > 0 {
+            fmt.Printf("Firestoreから読み取ったデータ: %v\n", data)
+
+        }
+    }
 }
