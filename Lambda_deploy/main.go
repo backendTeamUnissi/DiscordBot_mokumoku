890ac565a52deb7bbcd2f335cb20acb431e1d356
yuttto0024 <yuto0024@outlook.com>
2024-11-10 16:24:05 +0900

    Firestoreクライアントの設定および初期化

--- Lambda_deploy/main.go
+++ Lambda_deploy/main.go
@@ -1,6 +1,33 @@
 package main
-import()
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"cloud.google.com/go/firestore"
+	"github.com/joho/godotenv"
+	"google.golang.org/api/option"
+)
+
+var client *firestore.Client
 
 func main() {
-	// ここにコードを書く
+	// .envファイルから環境変数を読み込む
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf(".envファイルの読み込みに失敗しました: %v", err)
+	}
+
+	// Firestoreクライアントの設定、初期化
+	ctx := context.Background()
+	client, err = firestore.NewClient(ctx, "peachtech-mokumoku", option.WithCredentialsFile("./peachtech-mokumoku-91af9d3931c9.json"))
+	if err != nil {
+		log.Fatalf("Firestoreクライアントの作成に失敗しました: %v", err)
+	}
+	defer client.Close()
+
+	// クライアント作成成功時にメッセージ
+	fmt.Println("Firestoreクライアントの作成に成功しました")
+
 }
